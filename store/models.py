a7fe580b452f42f2f1ae8a517d4369dae91a24aa
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-23 22:11:29 +0300

    Create OrderItem model

--- store/models.py
+++ store/models.py
@@ -30,3 +30,12 @@ class Order(models.Model):
     def __str__(self) -> str:
         return f'Order for{self.customer}'
     
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,blank= True,null= True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,blank= True,null= True)
+    quantity = models.IntegerField(default=1,blank= True,null= True)
+    price = models.DecimalField(max_digits=10, decimal_places=2,blank= True,null= True)
+
+    def __str__(self):
+        return f'{self.product.title} ({self.quantity})'
