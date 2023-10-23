7afc659b4e44607ebd63754ba102413bb446bf2b
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-23 21:54:16 +0300

    Create Product model

--- store/models.py
+++ store/models.py
@@ -10,3 +10,13 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
     
+
+class Product(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False,null=True)
+
+    def __str__(self):
+
+        return self.name
+    
