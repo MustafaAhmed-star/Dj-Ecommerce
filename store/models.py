4d745e7ebdb34ce7c5bd556724e712b2bc4d6c98
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-23 22:03:44 +0300

    Create Order model

--- store/models.py
+++ store/models.py
@@ -15,8 +15,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True)
-
+    #image
     def __str__(self):
 
         return self.name
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,blank=True,null=True)
+    date_order = models.DateField(auto_now_add=True)
+    complete = models.BooleanField(default=False,blank=True,null=True)
+    transaction_id = models.CharField(max_length=200,null = True)
+
+
+    def __str__(self) -> str:
+        return f'Order for{self.customer}'
     
