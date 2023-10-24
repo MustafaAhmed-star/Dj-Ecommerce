40f40ff15400522e8125c81f38c8c40c644f1508
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-24 18:13:11 +0300

    create a scripts to create a products

---
+++ create_product.py
@@ -0,0 +1,27 @@
+def main():
+    fake: Faker = Faker()
+    product_names = ["T-shirt", "Jeans", "Sneakers", "Jacket", "Hat", "Socks", "Backpack", "Sweater", "Dress", "Skirt"]
+    for _ in range(5):
+        products =Product.objects.create(
+            name=random.choice(product_names),
+            price=random.uniform(100, 1000)
+        )
+        print(f"Created product. Name: {products.name}")
+    task_count = Product.objects.count()
+
+    print(f"There are {task_count} todos in the database")
+
+    
+ 
+if __name__ == "__main__":
+    import os
+ 
+    from django.core.wsgi import get_wsgi_application
+ 
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+    application = get_wsgi_application()
+    import random
+    from faker import Faker
+    from store.models import Product
+ 
+    main()
