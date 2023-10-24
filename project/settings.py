1ee9b088fa473f230f10f4300d40dea240393ef8
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-24 14:04:59 +0300

    confg media and static

--- project/settings.py
+++ project/settings.py
@@ -120,6 +120,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =[
     BASE_DIR /'static'
 ]
+STATIC_ROOT = "/static/"
+
+# MEDia files (uploaded by the user)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- store/models.py
+++ store/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True)
-    image = models.ImageField(null = True, blank = True)
+    image = models.ImageField(upload_to='media',null = True, blank = True)
     def __str__(self):
 
         return self.name
