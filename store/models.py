316e6e2b34839def87ecae42247d31387cb48af4
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-24 14:19:27 +0300

    handle error when there no image for a product

--- store/models.py
+++ store/models.py
@@ -15,10 +15,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True)
-    image = models.ImageField(upload_to='media',null = True, blank = True)
+    image = models.ImageField(null = True, blank = True)
     def __str__(self):
 
         return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url  = ""
+        return url
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,blank=True,null=True)
