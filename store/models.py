1c1b41ce47b3d4ec4a20cb3862b44a0cb9c8e02e
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-24 12:51:17 +0300

    create shippingadrees model then migrate all models

--- store/models.py
+++ store/models.py
@@ -35,7 +35,20 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE,blank= True,null= True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE,blank= True,null= True)
     quantity = models.IntegerField(default=1,blank= True,null= True)
-    price = models.DecimalField(max_digits=10, decimal_places=2,blank= True,null= True)
-
+    date_added = models.DateField(auto_now_add=True)
     def __str__(self):
         return f'{self.product.title} ({self.quantity})'
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank= True)
+    order  = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    address = models.CharField(max_length=200,null=True)
+    city = models.CharField(max_length=200,null=True)
+    state = models.CharField(max_length=200,null=True)
+    zipcode = models.CharField(max_length=200,null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.address
