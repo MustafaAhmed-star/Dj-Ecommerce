c776e9002d9fef4b0d42f8b7b4af7ead973a0dbd
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-26 11:19:55 +0300

    add logic for solve when the user in not auth

--- store/views.py
+++ store/views.py
@@ -15,6 +15,7 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
 
     context = {'items':items,'order':order}
     return render(request,'store/cart.html', context)
