080d731a285d880c9555d6bdbdebc64357060478
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-29 20:38:48 +0200

    Render cart item total in icon navabar(this logic not efficent i will change it later)

--- store/views.py
+++ store/views.py
@@ -5,8 +5,17 @@ from django.http import JsonResponse
 # Create your views here.
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems =['get_cart_items']
     products  =Product.objects.all
-    context = {'products':products}
+    context = {'products':products,'cartItems':cartItems}
 
     return render(request,'store/store.html', context)
 
