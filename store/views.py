8ea82c2ce5cee790d06003ac325ba110d5f3ec58
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-10-29 20:43:42 +0200

    solve a cart total doesnt appear in all urls (this one off disadavantage of this logic )

--- store/views.py
+++ store/views.py
@@ -24,11 +24,13 @@ def cart(request):
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems =['get_cart_items']
 
-    context = {'items':items,'order':order}
+    context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,'store/cart.html', context)
 
 def checkout(request):
@@ -36,10 +38,13 @@ def checkout(request):
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
-    context = {'items':items,'order':order}
+        cartItems =['get_cart_items']
+
+    context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,'store/checkout.html', context)
 
 
