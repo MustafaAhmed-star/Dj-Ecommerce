d5787bd34d5d6ab09a9ae78d48b7049c3ddd11f7
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-06 23:36:08 +0200

    add &reomve items

--- static/js/cart.js
+++ static/js/cart.js
@@ -7,8 +7,7 @@ for (var i = 0; i < updateBtns.length; i++) {
         console.log('productId: ' + productId, 'action: ' + action);
         console.log('USER:' + user )
         if (user=='AnonymousUser'){
-            console.log('Not Logged In');
-
+            addCookieItem(productId, action)
         }else{
             updateUserOrder(productId, action)
             
@@ -16,6 +15,38 @@ for (var i = 0; i < updateBtns.length; i++) {
 
     });
 }
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	
+	location.reload()
+}
+
+
+
+
+
+
 
 function updateUserOrder(productId,action){
 
@@ -38,3 +69,4 @@ function updateUserOrder(productId,action){
         location.reload()
     });
 }
+
