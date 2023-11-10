0f5743f3cd4e88ea90413276b3bac2836b1c41a2
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-10 22:33:28 +0200

    complete cart items and handle a error

--- store/views.py
+++ store/views.py
@@ -45,23 +45,27 @@ def cart(request):
         order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
         cartItems =order['get_cart_items']
         for i in cart:
-            cartItems+= cart[i]['quantity'] 
-            product = Product.objects.get(id=i)
-            total = (product.price *cart[i]['quantity'])
-            order['get_cart_total'] += total
-            order['get_cart_items']+=cart[i]['quantity']
-            item={
-                 'product':{
-                      'id' : product.id,
-                      'name': product.name,
-                      'price': product.price,
-                      'imageURL': product.imageURL,   
-                 },
-                 'quantity':cart[i]['quantity'],
-                 'get_total':total,
-            }
-            items.append(item)
-            
+            try:
+                cartItems+= cart[i]['quantity'] 
+                product = Product.objects.get(id=i)
+                total = (product.price *cart[i]['quantity'])
+                order['get_cart_total'] += total
+                order['get_cart_items']+=cart[i]['quantity']
+                item={
+                    'product':{
+                        'id' : product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL,   
+                    },
+                    'quantity':cart[i]['quantity'],
+                    'get_total':total,
+                }
+                items.append(item)
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass    
 
     context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,'store/cart.html', context)
