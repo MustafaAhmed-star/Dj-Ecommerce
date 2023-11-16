51b0c3428e74468d591f4ff12212e739a13d8c1d
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-16 12:51:58 +0200

    cleanCode ..create a def guestOrder in utils.py

--- store/utils.py
+++ store/utils.py
@@ -43,6 +43,7 @@ def cookieCart(request):
 
 
 def cartData(request):
+
 	if request.user.is_authenticated:
 		customer = request.user.customer
 		order,created = Order.objects.get_or_create(customer=customer,complete=False)
@@ -54,3 +55,32 @@ def cartData(request):
 		order =  cookieData['order']
 		items = cookieData['items'] 
 	return {'cartItems':cartItems ,'order':order, 'items':items}
+	
+	
+	
+def guestOrder(request, data):
+	name = data['form']['name']
+	email = data['form']['email']
+
+	cookieData = cookieCart(request)
+	items = cookieData['items']
+
+	customer, created = Customer.objects.get_or_create(
+			email=email,
+			)
+	customer.name = name
+	customer.save()
+
+	order = Order.objects.create(
+		customer=customer,
+		complete=False,
+		)
+
+	for item in items:
+		product = Product.objects.get(id=item['id'])
+		orderItem = OrderItem.objects.create(
+			product=product,
+			order=order,
+			quantity=(item['quantity'] if item['quantity']>0 else -1*item['quantity']), # negative quantity = freebies
+		)
+	return customer, order
